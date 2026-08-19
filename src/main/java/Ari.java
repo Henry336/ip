c3271243da979a8bbaa4576eb3c4c27a12de7e52
Hein Lin Htet <heinlinhtet3306@gmail.com>
2026-08-19 22:38:38 +0800

    Add 'Add' feature and 'list' command

--- src/main/java/Ari.java
+++ src/main/java/Ari.java
@@ -1,11 +1,30 @@
 import java.util.Scanner;
 
 public class Ari {
+    public static int idx = 0;
+    public static String[] list = new String[100];
+    public static String GOODBYE = "bye";
+    public static String LIST = "list";
+
     public static void print(String s) {
         System.out.println(s);
     }
 
+    public static void printList(String[] list) {
+        int init = 0;
+        String result = "";
+
+        while (init < idx) {
+            int num = init + 1;
+            result += String.format("%s. %s\n", num, list[init]);
+            init += 1;
+        }
+
+        print(result);
+    }
+
     public static void main(String[] args) {
+
         String banner =
                 "   ----   \n"
                 + "  / /\\ \\ \n"
@@ -25,11 +44,19 @@ public class Ari {
         String userInput = scanner.nextLine();
         String flatInput = userInput.toLowerCase();
 
-        while (!flatInput.equals("bye")) {
-            print(userInput);
+        while (!flatInput.equals(GOODBYE)) {
+            if (!flatInput.equals(LIST)) {
+                list[idx] = userInput;
+                String addedText = String.format("added: %s", userInput);
+                print(addedText);
+                idx += 1;
+            } else {
+                printList(list);
+            }
             userInput = scanner.nextLine();
             flatInput = userInput.toLowerCase();
         }
+
         print(farewell);
         scanner.close(); // always clean up
     }
