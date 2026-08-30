df9c2cef5ef0ec5adce56ea80fef3e38d92928c3
Hein Lin Htet <heinlinhtet3306@gmail.com>
2026-08-30 18:02:03 +0800

    Replace AriTest with ParserTest and TaskListTest

---
+++ src/test/java/ari/task/TaskListTest.java
@@ -0,0 +1,70 @@
+package ari.task;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import ari.exception.TaskNotFoundException;
+
+import org.junit.jupiter.api.Test;
+
+public class TaskListTest {
+    @Test
+    public void markTask_validSecondId_marksSecondTaskOnly() throws TaskNotFoundException {
+        TaskList tasks = new TaskList();
+        tasks.addTask(new TodoTask("first"));
+        tasks.addTask(new TodoTask("second"));
+
+        String markedTask = tasks.markTask(2);
+
+        assertEquals("[T][X] second", markedTask);
+        assertEquals("[T][ ] first", tasks.getTask(0).toString());
+        assertEquals("[T][X] second", tasks.getTask(1).toString());
+    }
+
+    @Test
+    public void markTask_zeroId_throwsTaskNotFoundException() {
+        TaskList tasks = createListWithOneTask();
+
+        assertMarkTaskFails(tasks, 0);
+    }
+
+    @Test
+    public void markTask_negativeId_throwsTaskNotFoundException() {
+        TaskList tasks = createListWithOneTask();
+
+        assertMarkTaskFails(tasks, -1);
+    }
+
+    @Test
+    public void markTask_idBeyondListSize_throwsTaskNotFoundException() {
+        TaskList tasks = createListWithOneTask();
+
+        assertMarkTaskFails(tasks, 2);
+    }
+
+    @Test
+    public void markTask_emptyList_throwsTaskNotFoundException() {
+        TaskList tasks = new TaskList();
+
+        assertMarkTaskFails(tasks, 1);
+    }
+
+    private TaskList createListWithOneTask() {
+        TaskList tasks = new TaskList();
+        tasks.addTask(new TodoTask("only task"));
+        return tasks;
+    }
+
+    private void assertMarkTaskFails(TaskList tasks, int id) {
+        try {
+            tasks.markTask(id);
+            fail();
+        } catch (TaskNotFoundException e) {
+            String expectedMessage = String.format(
+                    "Task %s does not exist!\nSend 'list' to see which tasks you have left!",
+                    id
+            );
+            assertEquals(expectedMessage, e.getMessage());
+        }
+    }
+}
